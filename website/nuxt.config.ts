46df1d96dbf4a7de83c9bd13bf944324d236f679
zernonia <zernonia@gmail.com>
2023-05-05 08:45:25 +0800

    change from static to prerender

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    "/components/**": { static: true },
+    "/components/**": { prerender: true },
   },
   nitro: {
     rootDir: ".",
