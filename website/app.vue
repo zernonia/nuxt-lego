0f221b9a3ef6b38da842eeec88ff1ae2f8cf0bf9
zernonia <zernonia@gmail.com>
2023-05-05 13:50:28 +0800

    add loading indicator

--- website/app.vue
+++ website/app.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div class="text-gray-700 max-w-screen-xl mx-auto">
+    <NuxtLoadingIndicator color="rgb(96,165,250)" />
     <NavBar
       class="sticky top-0 bg-white bg-opacity-50 backdrop-blur-md"
     ></NavBar>
