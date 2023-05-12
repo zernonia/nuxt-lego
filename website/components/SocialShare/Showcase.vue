74872f84a35331bae11b74e37eac3ce33ebe9de2
megatheikal <64621442+megatheikal@users.noreply.github.com>
2023-05-13 00:20:12 +0800

    refactor: update Social Share showcase

--- website/components/SocialShare/Showcase.vue
+++ website/components/SocialShare/Showcase.vue
@@ -8,6 +8,7 @@
         <Icon name="mdi:facebook" />
         <Icon name="mdi:linkedin" />
         <Icon name="mdi:share-variant" />
+        <Icon name="mdi:reddit" />
       </div>
     </template>
 
