d426b2c24da48368bc991f22a2c49a2273ecc620
zernonia <zernonia@gmail.com>
2023-05-13 22:31:47 +0800

    refactor: grid missing, og render error

--- website/components/islands/CustomOgImage.vue
+++ website/components/islands/CustomOgImage.vue
@@ -5,27 +5,29 @@ defineProps<{ title: string; description: string }>();
 <template>
   <div
     :style="{ fontFamily: 'Plus Jakarta Sans' }"
-    class="w-full h-full flex flex-col bg-white text-blue-400 items-center justify-center"
+    class="relative w-full h-full flex flex-col bg-white text-blue-400 items-center justify-center"
   >
     <div
-      class="max-w-[600px] flex flex-col text-center items-center justify-center"
+      class="mt-26 max-w-[600px] flex flex-col text-center items-center justify-center"
     >
       <h1 class="text-7xl font-bold">{{ title }}</h1>
       <p class="text-3xl">{{ description }}</p>
     </div>
-    <div class="mt-20 flex items-center">
+    <div class="mt-28 flex flex-row items-center">
       <img src="/logo.png" height="50px" />
-      <span class="text-gray-800 font-bold ml-4 text-3xl">NuxtLego</span>
+      <p class="text-gray-800 font-bold ml-4 text-3xl">NuxtLego</p>
     </div>
-    <img class="fixed top-0 left-0 w-96 h-auto" src="/assets/img/grid.png" />
-    <img class="fixed top-0 right-0 w-96 h-auto" src="/assets/img/grid.png" />
+    <img class="absolute top-0 left-0 w-96" height="200px" src="/grid.png" />
+    <img class="absolute top-0 right-0 w-96" src="/grid.png" />
     <img
-      class="fixed bottom-0 left-0 w-96 h-auto rotate-180"
-      src="/assets/img/grid.png"
+      class="absolute bottom-0 left-0 w-96"
+      style="transform: rotate(180deg)"
+      src="/grid.png"
     />
     <img
-      class="fixed bottom-0 right-0 w-96 h-auto rotate-180"
-      src="/assets/img/grid.png"
+      class="absolute bottom-0 right-0 w-96"
+      style="transform: rotate(180deg)"
+      src="/grid.png"
     />
   </div>
 </template>
