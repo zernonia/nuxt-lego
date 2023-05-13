4ebe344f3beb3a66c467aa21056313a77172dc63
zernonia <zernonia@gmail.com>
2023-05-13 18:05:22 +0800

    feat: add island component for testing

---
+++ website/components/islands/CustomOgImage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{ title: string; description: string }>();
+</script>
+
+<template>
+  <div
+    class="w-full h-full flex text-white bg-blue-500 items-center justify-center"
+  >
+    <h1 :style="{ fontSize: '70px' }">{{ title }} 👋</h1>
+  </div>
+</template>
