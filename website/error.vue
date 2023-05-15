14f11ad5a2d3b91fe9e9705b936fafba1fe3253a
zernonia <zernonia@gmail.com>
2023-05-15 15:36:38 +0800

    chore: add custom 404 page

---
+++ website/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full">
+    <div class="my-32 flex flex-col items-center justify-center ">
+      <Icon name="mdi-map-marker-question-outline" class="text-4xl text-gray-300 mr-2" />
+      <h1 class="inline-flex text-5xl font-bold my-8">
+        {{ error.message }}
+      </h1>
+
+      <button class=" px-6 py-3 font-semibold rounded-lg border" @click="handleError">
+        Back to Home
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  error: { message: string }
+}>()
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
