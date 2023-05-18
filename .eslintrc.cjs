8279a42ec1f7d5da9c330a1b7e6a0b27c1d9cc3f
zernonia <zernonia@gmail.com>
2023-05-18 10:24:42 +0800

    chore: add antfu eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,3 @@
 module.exports = {
-  root: true,
-  extends: ["@nuxt/eslint-config"],
-  rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/no-template-shadow": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/no-v-html": "off",
-    "vue/no-v-text-v-html-on-component": "off",
-  },
-};
+  extends: '@antfu',
+}
