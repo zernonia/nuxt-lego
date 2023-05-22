36d7f821785974257e8f040c36aec8de646aa110
zernonia <zernonia@gmail.com>
2023-05-22 13:36:53 +0800

    feat: marquee component

---
+++ layer/components/Lego/MarqueeBanner/Right.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { rootKey } from './.keys'
+import { useMarqueeBanner } from './.composables'
+
+const rootInject = inject(rootKey, {
+  width: ref(0),
+  speed: 1,
+  hoveredSpeed: 0.5,
+})
+
+const el = ref<HTMLElement[]>()
+const offset = ref(rootInject.width.value)
+const intervalId = ref()
+
+const { componentsWidth, baseEl, shadowEl, shadowCount, isHovering, onMouseEnter, onMouseLeave } = useMarqueeBanner(el)
+
+onMounted(() => {
+  intervalId.value = setInterval(() => {
+    if (Math.abs(offset.value) >= (componentsWidth.value + rootInject.width.value)) {
+      offset.value = rootInject.width.value
+    }
+
+    else {
+      if (isHovering.value)
+        offset.value += rootInject.hoveredSpeed
+
+      else
+        offset.value += rootInject.speed
+    }
+  }, 10)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId.value)
+})
+</script>
+
+<template>
+  <div style="display: flex; position: relative; " :style="{ transform: `translateX(${offset}px)`, right: `${(componentsWidth * (shadowCount + 1))}px` }" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <component :is="component" v-for="(component, index) in shadowEl" :key="index" />
+    <component :is="component" v-for="(component, index) in baseEl" ref="el" :key="index" />
+  </div>
+</template>
---
+++ layer/components/Lego/MarqueeBanner/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { provide, ref } from 'vue'
+import { rootKey } from './.keys'
+
+const props = withDefaults(defineProps<{
+  speed: number
+  hoveredSpeed: number
+}>(), {
+  speed: 1,
+  hoveredSpeed: 0.5,
+})
+
+const el = ref<HTMLDivElement>()
+const { width } = useElementBounding(el)
+
+provide(rootKey, {
+  width,
+  ...props,
+})
+</script>
+
+<template>
+  <div
+    ref="el"
+    class="marquee-container"
+  >
+    <div v-if="width">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.marquee-container {
+  overflow: hidden;
+
+}
+</style>
