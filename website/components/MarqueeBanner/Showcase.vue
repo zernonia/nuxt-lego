e457ab5a3d17afbfc6820b85f6398c33e8942aa0
zernonia <zernonia@gmail.com>
2023-05-24 09:21:49 +0800

    chore: add marquee banner showcase

---
+++ website/components/MarqueeBanner/Showcase.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <ShowcaseCard to="/docs/components/marquee-banner">
+    <template #image>
+      <div class="overflow-hidden fade-edge w-full">
+        <div class="flex">
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+        </div>
+        <div class="flex mt-4 -ml-16">
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+        </div>
+      </div>
+    </template>
+
+    <h5 class="font-semibold">
+      Marquee Banner
+    </h5>
+    <p class="text-sm mt-1 text-gray-400">
+      Simple and easy marquee effect.
+    </p>
+  </ShowcaseCard>
+</template>
