f3ceece013537b74280d84a9068ccc310bbb2e6d
zernonia <zernonia@gmail.com>
2023-05-24 09:29:44 +0800

    fix: improve styling for marquee item

--- website/components/MarqueeBanner/Showcase.vue
+++ website/components/MarqueeBanner/Showcase.vue
@@ -6,15 +6,15 @@
     <template #image>
       <div class="overflow-hidden fade-edge w-full">
         <div class="flex">
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
         </div>
         <div class="flex mt-4 -ml-16">
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
-          <div class="flex-shrink-0 bg-white mr-2 w-32 h-10 border hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
+          <div class="flex-shrink-0 bg-gray-50 mr-2 w-32 h-10 border hover:bg-white hover:border-blue-400 transition rounded-full" />
         </div>
       </div>
     </template>
