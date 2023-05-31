998b31ee6ac48b402a32ecc7db84b6ca87296ca4
zernonia <zernonia@gmail.com>
2023-05-31 22:54:38 +0800

    feat: add blue prints container

---
+++ website/components/Blueprints/Container.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="not-prose w-full rounded-2xl min-h-lg border flex items-center justify-center bg-grid  bg-repeat-x">
+    <slot />
+  </div>
+</template>
