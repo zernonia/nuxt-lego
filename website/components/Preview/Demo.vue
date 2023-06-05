17ff1408c9c81e846402f4c9ba69ed3e1d8abf9a
zernonia <zernonia@gmail.com>
2023-06-05 14:49:25 +0800

    feat: add preview demo

--- website/components/Preview/Demo.vue
+++ website/components/Preview/Demo.vue
@@ -1,20 +1,43 @@
 <script setup lang="ts">
+import { withBase } from 'ufo'
+import { computed, useRuntimeConfig } from '#imports'
+
+// props from ProseImg (https://github.com/nuxt/content/blob/main/src/runtime/components/Prose/ProseImg.vue)
+const props = defineProps({
+  src: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+})
+
+const refinedSrc = computed(() => {
+  if (props.src?.startsWith('/') && !props.src.startsWith('//'))
+    return withBase(props.src, useRuntimeConfig().app.baseURL)
+
+  return props.src
+})
 </script>
 
 <template>
   <div>
     <LegoPreview class="transition-transform ease-in-out duration-500 outline-none">
-      <!-- Pass -->
-      <!-- <slot /> -->
-
-      <img
-        src="https://images.unsplash.com/photo-1515268064940-5150b7c29f35?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&w=800&q=80"
-        class="w-full rounded-2xl" alt=""
-      >
-
       <LegoPreviewDialog
         class="fixed z-800 left-0 top-0 w-screen h-screen bg-white bg-opacity-50 backdrop-blur-md"
       />
+
+      <img class="rounded-2xl" :src="refinedSrc" :alt="alt" :width="width" :height="height">
     </LegoPreview>
   </div>
 </template>
