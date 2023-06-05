0c391ae41c35cc575946ab47d6ed926697139ade
zernonia <zernonia@gmail.com>
2023-06-05 23:08:09 +0800

    feat: auto populate metadata for showcase card

--- website/components/ShowcaseCard.vue
+++ website/components/ShowcaseCard.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-defineProps<{
-  to: string
+const props = defineProps<{
+  slug: string
 }>()
+const { slug } = toRefs(props)
+
+const { data } = await useAsyncData(`showcase-${slug.value}`, () => queryContent(`/docs/components/${slug.value}`).only(['title', 'description', '_path', 'new']).findOne())
 </script>
 
 <template>
-  <NuxtLink :to="to">
+  <NuxtLink :to="data._path">
     <div class="group relative border hover:border-blue-400 transition rounded-xl overflow-hidden h-full">
       <div
         class="h-48 relative flex items-center justify-center bg-grid bg-no-repeat bg-cover"
@@ -17,7 +20,19 @@ defineProps<{
       </div>
 
       <div class="p-4">
-        <slot> text </slot>
+        <h5 class="font-semibold">
+          {{ data.title }}
+        </h5>
+        <p class="text-sm mt-1 text-gray-400">
+          {{ data.description }}
+        </p>
+
+        <span
+          v-if="data.new"
+          class="absolute top-4 right-4 text-xs px-2 flex items-center rounded-full bg-blue-50 text-blue-400"
+        >
+          New
+        </span>
       </div>
     </div>
   </NuxtLink>
